0970394e4031fd048565790780b354fa12c5ecbc
Erik Higginbotham <higgi615@ufl.edu>
2018-07-19 13:53:16 -0400

    added some folder structure

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var express = require("express");
 var app = express();
 var router = express.Router();
-var path = __dirname + "/";
+var path = require("path");
 
 router.use(function(req, res, next) {
   console.log("/" + req.method);
@@ -9,20 +9,26 @@ router.use(function(req, res, next) {
 });
 
 router.get("/", function(req, res) {
-  res.sendFile(path + "Page1.html");
+  res.sendFile(__dirname + "/" + "Page1.html");
+});
+
+router.get("/page1", function(req, res) {
+  res.sendFile(__dirname + "/" + "Page1.html");
 });
 
 router.get("/page2", function(req, res) {
-  res.sendFile(path + "Page2.html");
+  res.sendFile(__dirname + "/" + "Page2.html");
 });
 
 router.get("/card", function(req, res) {
-  res.sendFile(path + "card.html");
+  res.sendFile(__dirname + "/" + "card.html");
 });
 
 router.get("/fastcash", function(req, res) {
-  res.sendFile(path + "fastcash.html");
+  res.sendFile(__dirname + "/" + "fastcash.html");
 });
+
+app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/", router);
 
