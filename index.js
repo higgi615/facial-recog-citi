5119f57a498ec2c157b9c35d0cdd575a594cf31e
Erik Higginbotham <higgi615@ufl.edu>
2018-07-19 11:58:18 -0400

    added server/routing changes

--- index.js
+++ index.js
@@ -1,10 +1,31 @@
-const express = require("express");
-const app = express();
+var express = require("express");
+var app = express();
+var router = express.Router();
+var path = __dirname + "/";
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
+router.use(function(req, res, next) {
+  console.log("/" + req.method);
+  next();
 });
 
-app.listen(8000, () => {
-  console.log("Example app listening on port 8000!");
+router.get("/", function(req, res) {
+  res.sendFile(path + "Page1.html");
+});
+
+router.get("/page2", function(req, res) {
+  res.sendFile(path + "Page2.html");
+});
+
+router.get("/card", function(req, res) {
+  res.sendFile(path + "card.html");
+});
+
+router.get("/fastcash", function(req, res) {
+  res.sendFile(path + "fastcash.html");
+});
+
+app.use("/", router);
+
+app.listen(8000, function() {
+  console.log("Live at Port 8000");
 });
